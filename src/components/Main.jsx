29f779c5b608a752ae61b1af2fba4d654c797f05
Abhijay <abhijaysc19@gmail.com>
2023-12-03 14:11:05 +0530

    first commit till resume download option

---
+++ src/components/Main.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function click(){
+    window.location.href='https://drive.google.com/uc?export=download&id=1M9E0sLq4eFf42rTJY9SPpay84UyAZzJ9';
+}
+
+function Main(){
+    return(
+        <div className="main">
+            <h1>Candidate Details</h1>
+            <h3>Abhijay Sekhar Choudhury</h3>
+            <h3>B.Tech July 2024</h3>
+            <h3>Skills worked with : </h3>
+            <ul>
+                <li>GitHub Actions | Automation</li>
+                <li>HTML | CSS | JavaScript</li>
+                <li>ExpressJS | NodeJS</li>
+                <li>Python | JAVA</li>
+                <li>Rest-APIs</li>
+                <li>Django</li>
+                <li>ReactJS</li>
+                <li>PostgreSQL | MongoDB</li>
+                <li>Axios | Bootstrap</li>
+            </ul>
+   
+
+            <button className='button' onClick={click} type="button">Download Resume</button>
+            
+        </div>
+    )
+}
+
+export default Main;
